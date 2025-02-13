9021490fa29cef30b01793ceaa57bd643777117f
Khayalala <khayala.jafar@gmail.com>
2025-02-13 17:51:09 +0400

    Initial commit

---
+++ api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const api = axios.create({
+  baseURL: "http://localhost:5001"
+});
+export const fetchList = async () => {
+  return api
+    .get("/list")
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log("Error fetching data", error);
+      throw error;
+    })
+    .finally(() => {
+      console.log("Fetch attempt completed");
+    });
+};
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import Dropdown from "./components/Dropdown"
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Dropdown></Dropdown>
+    </>
+  )
+}
+
+export default App
